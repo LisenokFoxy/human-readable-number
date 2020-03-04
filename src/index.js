173a272623567750ca5237e90c3a7c57268792f0
Tatyana Pashintseva <Tatyana.Pashintseva@kcell.kz>
2020-03-04 12:53:47 +0600

    feat: add solution to human readable numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function toReadable (number) {
+export default function toReadable (number) {
+    let digits=number.toString().split("");
+    let ones =[" ", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    let tens=[" " ," ", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    let hundred= " hundred"; 
+    let thousand= "thousand";
+
+    if (number==0) {
+        return "zero";
+    }
+
+    if (number<20) {
+        return ones[number];
+    }
+
+    if (number>=20 && number<100) {
+       
+        return tens[digits[0]] + ones[digits[1]];
+    }
+
+    if (number>=100) {
+        return ones[digits[0]] + hundred + tens[digits[1]] + ones[digits[2]]; 
+    }
+    }
+
   
-}
